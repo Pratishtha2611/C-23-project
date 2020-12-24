9a6a64788d025de6017015620a915950e6dea088
Pratishtha2611 <74613734+Pratishtha2611@users.noreply.github.com>
2020-12-24 23:29:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,78 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 800);
+	rectMode(CENTER);
+	
+     var package_options = {
+		 restitution : 1.0
+	 }
+	packageSprite=createSprite(width/2, 80, 10,10,package_options);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor = color(255)
+
+
+	wood1 = createSprite(400,700,200,20);
+	wood1.shapeColor= ("red")
+
+	wood2 = createSprite(300,660,20,100);
+	wood2.shapeColor= ("red")
+
+	wood3 = createSprite(500,660,20,100);
+	wood3.shapeColor= ("red")
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	World.add(world, packageBody);
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  //wood1.display();
+  drawSprites();
+  keyPressed();
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.0, isStatic:false});
+    World.add(world, packageBody);
+    Engine.run(engine);
+  }
+}
+
+
+
